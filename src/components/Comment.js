9b191c6c0d4eb709747897dac78a526cd4b5eb3a
thatdania@gmail.com <thatdania@gmail.com>
2018-01-25 18:14:42 +0000

    Wrote code for comment component. Test passes
    All that Comment has is rendering a list item inside the component class

---
+++ src/components/Comment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render() {
+    return (
+      <li className='comment-item'>
+        <span>I am a comment</span>
+      </li>
+    )
+  }
+}
+
+export default Comment;
