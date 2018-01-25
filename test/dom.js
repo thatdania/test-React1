4ec75dc61f45606e2bad2875ebd6ce7819ed0c25
thatdania@gmail.com <thatdania@gmail.com>
2018-01-25 18:10:03 +0000

    Sucessfully run the test framework

--- test/dom.js
+++ test/dom.js
@@ -4,6 +4,8 @@ const {document} = (new JSDOM('<!doctype html><html><body></body></html>')).wind
 global.document = document;
 global.window = document.defaultView;
 
+var exposedProperties = ['window', 'navigator', 'document'];
+
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
